800ec0ebf1cd04a6f8da90febdf41c0892a5150e
Nawal Rai Chetan <152277551+PunditWhoCodes@users.noreply.github.com>
2025-05-10 13:22:20 +0500

    updated UI

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { fb, insta, twitter, yt} from '../assets/icons'
+import { fb, insta, twitter, yt } from '../assets/icons'
 const Footer = () => {
   return (
-    <section className="w-full bg-black text-white flex flex-col lg:flex-row justify-between items-start gap-10 lg:gap-4 px-10 py-14 lg:px-20 lg:py-28">
-      <div className="flex flex-col justify-center items-start gap-2 w-full lg:w-[40%]"> 
+    <section className="w-full bg-black text-white flex flex-col lg:flex-row justify-between items-start gap-10 lg:gap-4 px-10 py-14 lg:px-20 lg:py-28 z-10">
+      <div className="flex flex-col justify-center items-start gap-2 w-full lg:w-[40%]">
         <h1 className="text-green-600 font-bold text-4xl ">Nawal's CodeLab</h1>
         <p className="text-lg text-slate-200 mt-2">Where creativity meets innovation! Unleash your coding potential and embark on a journey of endless possibilities.</p>
         <div className="flex justify-center items-center gap-4 mt-7" id="logos">
@@ -25,19 +25,37 @@ const Footer = () => {
       <div className="flex flex-col justify-start lg:items-start w-full lg:w-[20%]">
         <ul className="flex flex-col justify-center items-start gap-3">
           <h1 className="text-xl font-semibold uppercase text-white">pages</h1>
-          <li className="text-slate-300 cursor-pointer hover-text-green-300"> Home
+          <li >
+            <a
+              href="#home"
+              className="text-slate-300 cursor-pointer hover-text-green-300"
+            >
+              Home
+            </a>
           </li>
-          <li className="text-slate-300 cursor-pointer hover-text-green-300"> Courses
+          <li >
+            <a href="#courses" className="text-slate-300 cursor-pointer hover-text-green-300" >
+              Courses
+            </a>
           </li>
-          <li className="text-slate-300 cursor-pointer hover-text-green-300"> Testimonials
+          <li>
+            <a href="#testimonials" className="text-slate-300 cursor-pointer hover-text-green-300">
+              Testimonials
+            </a>
           </li>
-          <li className="text-slate-300 cursor-pointer hover-text-green-300"> Pricing
+          <li>
+            <a href="#pricing" className="text-slate-300 cursor-pointer hover-text-green-300" >
+              Pricing
+            </a>
           </li>
-          <li className="text-slate-300 cursor-pointer hover-text-green-300"> Contact
+          <li>
+            <a href="#contact" className="text-slate-300 cursor-pointer hover-text-green-300" >
+              Contact
+            </a>
           </li>
         </ul>
       </div>
- 
+
     </section>
   );
 };
